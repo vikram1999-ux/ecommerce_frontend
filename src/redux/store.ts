406ba9902b62566fc46a180c6fbbfc597baf6d37
vikram mahara <vmahara1999@gmail.com>
2025-03-10 17:39:58 +0530

    changes

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -3,27 +3,21 @@ import { productAPI } from "./api/productAPI";
 import { userAPI } from "./api/userAPI";
 import { userReducer } from "./reducer/userReducer";
 import { cartReducer } from "./reducer/cartReducer";
-import { orderApi } from "./api/orderAPI";
-import { dashboardApi } from "./api/dashboardAPI";
+import { orderAPI } from "./api/orderAPI";
+import { dashboardAPI } from "./api/dashboardAPI";
 
 export const server = import.meta.env.VITE_SERVER;
 
 export const store = configureStore({
-  reducer: {
-    [userAPI.reducerPath]: userAPI.reducer,
-    [productAPI.reducerPath]: productAPI.reducer,
-    [orderApi.reducerPath]: orderApi.reducer,
-    [dashboardApi.reducerPath]: dashboardApi.reducer,
-    [userReducer.name]: userReducer.reducer,
-    [cartReducer.name]: cartReducer.reducer,
-  },
-  middleware: (mid) => [
-    ...mid(),
-    userAPI.middleware,
-    productAPI.middleware,
-    orderApi.middleware,
-    dashboardApi.middleware,
-  ],
+    reducer: {
+        [userAPI.reducerPath]: userAPI.reducer,
+        [productAPI.reducerPath]: productAPI.reducer,
+        [orderAPI.reducerPath]: orderAPI.reducer,
+        [dashboardAPI.reducerPath]: dashboardAPI.reducer,
+        [userReducer.name]: userReducer.reducer,
+        [cartReducer.name]: cartReducer.reducer,
+    },
+    middleware: (mid) => [...mid(), userAPI.middleware, productAPI.middleware, orderAPI.middleware, dashboardAPI.middleware],
 });
 
 export type RootState = ReturnType<typeof store.getState>;
